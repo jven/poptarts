625d4b94a1cb334703f165633908e25d210707ca
Justin Venezuela <jven@jvenezue.la>
2019-01-26 17:52:34 -0500

    Adds items on the screen that don't do anything.

--- src/screen/js/game.ts
+++ src/screen/js/game.ts
@@ -1,5 +1,7 @@
 import Phaser = require('phaser');
 import { ControllerStateMap } from './controllerstatemap';
+import { Item } from './item/item';
+import { ItemType } from './item/itemtype';
 import { Player } from './player';
 
 export class Game {
@@ -13,11 +15,36 @@ export class Game {
 
   preload(scene: Phaser.Scene): void {
     scene.load.image('poptart', 'screen/img/poptart.png');
+    scene.load.image('poptartbox', 'screen/img/poptartbox.png');
+    scene.load.image('shower', 'screen/img/shower.png');
     scene.load.image('smiley', 'screen/img/smiley.png');
+    scene.load.image('toaster', 'screen/img/toaster.png');
   }
 
   create(scene: Phaser.Scene): void {
     this.player = new Player(scene.add.sprite(200, 200, 'smiley'));
+
+    new Item(
+        ItemType.POPTART_BOX,
+        scene.add.sprite(600, 400, 'poptartbox'),
+        {
+          width: 50,
+          height: 50
+        });
+    new Item(
+        ItemType.SHOWER,
+        scene.add.sprite(400, 100, 'shower'),
+        {
+          width: 80,
+          height: 100
+        });
+    new Item(
+        ItemType.TOASTER,
+        scene.add.sprite(300, 300, 'toaster'),
+        {
+          width: 50,
+          height: 50
+        });
   }
 
   update(scene: Phaser.Scene): void {
