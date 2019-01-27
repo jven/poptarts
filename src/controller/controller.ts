0de286a1c4c67a0535a06ca6b36253a2ab5be928
Justin Venezuela <jven@jvenezue.la>
2019-01-27 15:33:29 -0500

    Fixes the controller to work on a real device.

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -10,16 +10,16 @@ function controllerMain() {
   type elemId = string;
   type eventName = string;
   const buttonMap = new Map<[elemId, eventName], Intent>();
-  buttonMap.set(['up-btn', 'mousedown'], Intent.PRESS_UP);
-  buttonMap.set(['right-btn', 'mousedown'], Intent.PRESS_RIGHT);
-  buttonMap.set(['down-btn', 'mousedown'], Intent.PRESS_DOWN);
-  buttonMap.set(['left-btn', 'mousedown'], Intent.PRESS_LEFT);
-  buttonMap.set(['action-btn', 'mousedown'], Intent.PRESS_ACTION);
-  buttonMap.set(['up-btn', 'mouseup'], Intent.RELEASE_UP);
-  buttonMap.set(['right-btn', 'mouseup'], Intent.RELEASE_RIGHT);
-  buttonMap.set(['down-btn', 'mouseup'], Intent.RELEASE_DOWN);
-  buttonMap.set(['left-btn', 'mouseup'], Intent.RELEASE_LEFT);
-  buttonMap.set(['action-btn', 'mouseup'], Intent.RELEASE_ACTION);
+  buttonMap.set(['up-btn', 'touchstart'], Intent.PRESS_UP);
+  buttonMap.set(['right-btn', 'touchstart'], Intent.PRESS_RIGHT);
+  buttonMap.set(['down-btn', 'touchstart'], Intent.PRESS_DOWN);
+  buttonMap.set(['left-btn', 'touchstart'], Intent.PRESS_LEFT);
+  buttonMap.set(['action-btn', 'touchstart'], Intent.PRESS_ACTION);
+  buttonMap.set(['up-btn', 'touchend'], Intent.RELEASE_UP);
+  buttonMap.set(['right-btn', 'touchend'], Intent.RELEASE_RIGHT);
+  buttonMap.set(['down-btn', 'touchend'], Intent.RELEASE_DOWN);
+  buttonMap.set(['left-btn', 'touchend'], Intent.RELEASE_LEFT);
+  buttonMap.set(['action-btn', 'touchend'], Intent.RELEASE_ACTION);
   buttonMap.forEach((intent, [buttonId, event]) => {
     const button = document.getElementById(buttonId)!;
     button.addEventListener(event, () => {
